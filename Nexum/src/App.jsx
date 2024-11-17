c885ef5c903782fd89b58c25dc5b74ce2ba1d561
CaioViniciusPF <freirescaiovini@gmail.com>
2024-11-17 02:56:26 -0300

    adicionando estilos padrao de cor e texto e formatando o header

--- Nexum/src/App.jsx
+++ Nexum/src/App.jsx
@@ -1,9 +1,23 @@
 
 function App() {
   return (
-    <h1 className="text-4xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <header className="bg-neutral-800 text-whiteFont h-20 ">
+      <div className="w-full max-w-[1280px] my-0 mx-auto flex justify-between">
+        <img src="vite.svg" alt="logo" />
+        <nav>
+          <ul className="flex">
+            <li>Serviços</li>
+            <li>Casos</li>
+            <li>Depoimentos</li>
+            <li>Contato</li>
+            <li>Localização</li>
+          </ul>
+        </nav>
+        <button>Whatsapp</button>
+      </div>
+    </header>
+    </>
   )
 }
 
--- Nexum/tailwind.config.js
+++ Nexum/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+    "./index.html", "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'yellowBg':'#F7AD0C',
+        'whiteFont':'#EFEFEF'
+      },
+      fontFamily: {
+        Fraunces: ['Fraunces' , 'sans-serif'],
+        NunitoSans: ['NunitoSans' , 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
