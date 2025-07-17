3cf98219867f58f9f4a2efae84b618aba8ff4b08
fasnfilho <fabinhosnf@gmail.com>
2025-07-16 22:48:12 -0300

    Colocando a logo no header

--- Nexum/src/components/Header.jsx
+++ Nexum/src/components/Header.jsx
@@ -13,22 +13,7 @@ const Header = () => {
           <a href="/">
             <div className="bg-yellowBg w-12 h-12 flex items-center justify-center">
               {/* Placeholder para o ícone do logo */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="text-white"
-              >
-                <path d="M12 2L2 7l10 5 10-5-10-5z" />
-                <path d="M2 17l10 5 10-5" />
-                <path d="M2 12l10 5 10-5" />
-              </svg>
+              <img src="/logo.png" alt="logo-header" />
             </div>
           </a>
         </div>
